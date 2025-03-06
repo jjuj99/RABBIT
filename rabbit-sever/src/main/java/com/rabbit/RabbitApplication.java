99f1d8ad5119d3d807fd9109e18781af9ce902bf
문종하 <3477qq@naver.com>
2025-03-06 22:22:01 +0900

    init: 백엔드 프로젝트 스케폴딩
    - 프로젝트 디렉토리 구조 생성
    - 기본 의존성 패키지 설치
    - global 패키지 구현
      - config: Swagger 설정
      - exception: 비즈니스 예외 처리 및 글로벌 예외 핸들러
      - request/response: 페이징 및 API 응답 구조 정의
    - 로깅

---
+++ rabbit-sever/src/main/java/com/rabbit/RabbitApplication.java
@@ -0,0 +1,17 @@
+package com.rabbit;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@Slf4j
+@EnableScheduling
+public class RabbitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitApplication.class, args);
+	}
+
+}

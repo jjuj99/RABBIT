13536967e9c1ea8a22e4ea048428b75b14f51274
정유진 <yueonq@gmail.com>
2025-03-27 13:36:24 +0900

    test: jenkins push test

--- rabbit-server/src/main/java/com/rabbit/auth/controller/HelloController.java
+++ rabbit-server/src/main/java/com/rabbit/auth/controller/HelloController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class HelloController {
 
-    @GetMapping
+    @GetMapping("/test")
     public String test() {
         return "Test Jenkins! !";
     }

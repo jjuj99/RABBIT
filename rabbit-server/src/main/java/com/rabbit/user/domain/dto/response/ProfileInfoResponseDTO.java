b3c585416ce0ff9365a75565ef391888454c2468
정유진 <yueonq@gmail.com>
2025-03-28 11:28:09 +0900

    feat: 유저 마이페이지 정보 조회 api 구현

--- rabbit-server/src/main/java/com/rabbit/user/domain/dto/response/ProfileInfoResponseDTO.java
+++ rabbit-server/src/main/java/com/rabbit/user/domain/dto/response/ProfileInfoResponseDTO.java
@@ -3,15 +3,18 @@ package com.rabbit.user.domain.dto.response;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
-@Schema(description = "이메일로 유저 검색 응답 DTO")
+@Schema(description = "마이페이지 응답 DTO")
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class SearchUserResponseDTO {
+public class ProfileInfoResponseDTO {
 
     private int userId;
     private String email;
     private String userName;
     private String nickname;
+    private String bankName;
+    private String refundAccount;
+    private String walletAddress;
 }

5f2fbe1177e0e8ed87cc2ebbe359e1e6e91e0746
이가현 <lgh9776@naver.com>
2025-04-01 13:30:00 +0900

    feat: RABBIT 코인 충전 및 자동승인 메커니즘 구현

--- rabbit-contract/contracts/rabbit-coin/interfaces/ICustomERC20.sol
+++ rabbit-contract/contracts/rabbit-coin/interfaces/ICustomERC20.sol
@@ -8,6 +8,10 @@ import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
  * @dev IERC20Metadata를 확장하여 추가 기능(mint, burn)을 정의하는 인터페이스
  */
 interface ICustomERC20 is IERC20Metadata {
+
+    // 충전 이벤트
+    event Charged(address indexed account, uint256 amount);
+
     /**
      * @dev 토큰 양을 늘림 (Mint)
      * @param account 토큰을 받을 주소

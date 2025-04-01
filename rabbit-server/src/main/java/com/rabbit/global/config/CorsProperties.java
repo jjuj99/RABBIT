e51a9a4200c522e98d49d2dc5c82430f3e019450
정유진 <yueonq@gmail.com>
2025-04-01 16:26:43 +0900

    refactor: CORS 허용 origin을 applicatoin에서 불러오도록 변경

---
+++ rabbit-server/src/main/java/com/rabbit/global/config/CorsProperties.java
@@ -0,0 +1,14 @@
+package com.rabbit.global.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@ConfigurationProperties(prefix = "cors")
+@Data
+public class CorsProperties {
+    private List<String> allowedOrigins;
+}

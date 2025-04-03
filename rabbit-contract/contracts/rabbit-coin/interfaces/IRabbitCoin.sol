e8fdd5cec60723ea6bd15cd015b00f1c59d1d985
이가현 <lgh9776@naver.com>
2025-04-04 08:55:55 +0900

    feat: permit과 서명 추가 및 시스템 컨트랙트 삭제

--- rabbit-contract/contracts/rabbit-coin/interfaces/IRabbitCoin.sol
+++ rabbit-contract/contracts/rabbit-coin/interfaces/IRabbitCoin.sol
@@ -10,6 +10,22 @@ import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 interface IRabbitCoin is IERC20Metadata {
 
     /**
+    * @dev EIP-2612 서명을 통한 승인
+    * @param owner 토큰 소유자 주소
+    * @param spender 토큰 사용이 허용될 주소
+    * @param value 승인할 토큰 양
+    * @param deadline 서명 유효기간
+    * @param signature 서명 데이터
+    */
+    function permit(
+        address owner,
+        address spender,
+        uint256 value,
+        uint256 deadline,
+        bytes memory signature
+    ) external;
+
+    /**
      * @dev 토큰 양을 늘림 (Mint)
      * @param account 토큰을 받을 주소
      * @param amount 추가할 토큰 양
@@ -21,26 +37,4 @@ interface IRabbitCoin is IERC20Metadata {
      * @param amount 소각할 토큰 양
      */
     function burn(uint256 amount) external;
-
-    /**
-     * @dev 시스템 컨트랙트 주소 설정
-     * @param systemContract 시스템 컨트랙트 주소
-     */
-    function setSystemContract(address systemContract) external;
-    
-    /**
-     * @dev RAB 코인 충전
-     * @param account 충전할 계좌 주소
-     * @param amount 충전할 RAB 양
-     * @return 성공 여부
-     */
-    function charge(address account, uint256 amount) external returns (bool);
-    
-    /**
-     * @dev RAB 코인 환불
-     * @param account 소각할 계좌 주소
-     * @param amount 소각할 RAB 양
-     * @return 성공 여부
-     */
-    function refund(address account, uint256 amount) external returns (bool);
 }

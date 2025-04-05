37fb4b3da6012d689dd8bdd012b2850d0744b101
문종하 <3477qq@naver.com>
2025-04-05 13:01:02 +0900

    fix: SWAGGER 수정 및 "계약 취소" 관련 버그 수정

--- rabbit-server/src/main/java/com/rabbit/contract/domain/dto/request/ContractRejectRequestDTO.java
+++ rabbit-server/src/main/java/com/rabbit/contract/domain/dto/request/ContractRejectRequestDTO.java
@@ -1,5 +1,6 @@
 package com.rabbit.contract.domain.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -17,6 +18,7 @@ public class ContractRejectRequestDTO {
 
     @Schema(description = "계약 취소 여부", example = "false")
     @Builder.Default
+    @JsonProperty("isCanceled")
     private boolean isCanceled = false;
 
 //    @Schema(description = "패스 인증 토큰", example = "token123")

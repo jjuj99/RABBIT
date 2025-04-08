d8d5c20d8613b9247cda141c6283b32c1cf431ec
Youmin Kim <march02198@gmail.com>
2025-04-08 14:51:59 +0900

    fix: sse user 알림 구독 token 받게 수정

--- rabbit-server/src/main/java/com/rabbit/sse/controller/SseController.java
+++ rabbit-server/src/main/java/com/rabbit/sse/controller/SseController.java
@@ -1,9 +1,7 @@
 package com.rabbit.sse.controller;
 
-import com.rabbit.sse.controller.swagger.SseControllerSwagger;
-import com.rabbit.sse.domain.dto.request.SseRequestDTO;
+import com.rabbit.global.util.JwtUtil;
 import com.rabbit.sse.service.SseService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -15,14 +13,27 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 public class SseController {
 
     private final SseService sseService;
+    private final JwtUtil jwtUtil;
 
-    @SseControllerSwagger.SubscribeSseApi
-    @GetMapping(value = "/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public SseEmitter subscribe(
-            @Valid SseRequestDTO request,
+    // 인증이 필요한 개인 SSE
+    @GetMapping(value = "/subscribe/user", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public SseEmitter subscribeUser(
+            @RequestParam String token,
             @RequestHeader(value = "Last-Event-Id", required = false) String lastEventId
     ) {
-        String key = request.toKey();
+        String userId = jwtUtil.getUserIdFromToken(token);
+        String key = "user-" + userId;
         return sseService.subscribe(key);
     }
+
+    // 인증 없이 구독 가능한 경매 SSE
+    @GetMapping(value = "/subscribe/auction", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public SseEmitter subscribeAuction(
+            @RequestParam Long id,
+            @RequestHeader(value = "Last-Event-Id", required = false) String lastEventId
+    ) {
+        String key = "auction-" + id;
+        return sseService.subscribe(key);
+    }
+
 }

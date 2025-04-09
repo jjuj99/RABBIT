08a7ffabf51f631485d3892fba00465b7aed7947
정유진 <yueonq@gmail.com>
2025-04-09 12:28:17 +0900

    fix: allow origins 수정

--- rabbit-server/src/main/java/com/rabbit/global/config/CorsProperties.java
+++ rabbit-server/src/main/java/com/rabbit/global/config/CorsProperties.java
@@ -2,11 +2,12 @@ package com.rabbit.global.config;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-@Component
+@Configuration
 @ConfigurationProperties(prefix = "cors")
 @Data
 public class CorsProperties {

2384d48a56f63ab7bf5f380ca9cb74901b7a5d1d
박수양 <sktndid1203@naver.com>
2025-04-10 18:02:24 +0900

    feat : 나의 채무 수정

---
+++ rabbit-client/src/shared/ui/skeleton.tsx
@@ -0,0 +1,14 @@
+import { cn } from "@/shared/lib/utils";
+
+type SkeletonProps = React.HTMLAttributes<HTMLDivElement>;
+
+const Skeleton = ({ className, ...props }: SkeletonProps) => {
+  return (
+    <div
+      className={cn("animate-pulse rounded-md bg-gray-700/50", className)}
+      {...props}
+    />
+  );
+};
+
+export { Skeleton };

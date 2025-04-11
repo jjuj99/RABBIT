dbbb5107b8b3b9e0c0de6565d1971a982eb633b1
문종하 <3477qq@naver.com>
2025-04-11 11:10:59 +0900

    fix: url 경로 수정

--- rabbit-client/src/entities/auth/api/authApi.ts
+++ rabbit-client/src/entities/auth/api/authApi.ts
@@ -25,7 +25,7 @@ const VITE_API_VERSION = import.meta.env.VITE_API_VERSION;
 
 export const GetUserAPI = async (): Promise<ApiResponse<User>> => {
   const res = await fetch(
-    `${VITE_API_URL}/${VITE_API_VERSION}/user/me`,
+    `${VITE_API_URL}/${VITE_API_VERSION}/users/me`,
     fetchOption("GET", undefined, "access"),
   );
   const data = await res.json();
